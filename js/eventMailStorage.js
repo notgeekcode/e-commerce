aa5d44d66e58862e357bab2f84b09b049539bacd
M. N. Couto <nahuelcouto@gmail.com>
2022-09-04 17:21:55 -0300

    Mail y botones de filtrado
    Nuevos botones de filtrado para los productos.
    Además mostramos el mail del usuario en la barra de navegación

--- js/eventMailStorage.js
+++ js/eventMailStorage.js
@@ -1,3 +1,6 @@
-//tuve que creear un nuevo archivo js porque si vinculaba en un archivo js a dos html por alguna razon no se ejecutaba de forma correcta
-let listaPrevia1 = JSON.parse(localStorage.getItem("userEmail")??"[]"); //obtengo los datos del mail a traves del localstorage y con el JSON.parse lo transforme en String
-document.getElementById("nav-item-id").innerHTML = `<a class = "nav-link" href="my-profile.html"> ${listaPrevia1} </a>`; //ingreso a las clases="profileUsers" y alli pongo un hipervinculo con los datos del localStorage, y este hipervinculo lleva hacia my-profile.html
+let listaPrevia1 = JSON.parse(localStorage.getItem("userEmail")??"[]"); 
+//obtengo los datos del mail a traves del localstorage y con el JSON.parse lo transforme en String
+
+document.getElementById("nav-item-id").innerHTML = `<a class = "nav-link" href="my-profile.html"> ${listaPrevia1} </a>`; 
+//ingreso a las clases="profileUsers" y alli pongo un hipervinculo con los datos del localStorage, y este 
+//hipervinculo lleva hacia my-profile.html
--- js/products.js
+++ js/products.js
@@ -1,54 +1,151 @@
-//array donde se cargarán los datos recibidos:
-let categoriesArray = []; //declaro una lista vacia
- 
-//función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
-function showCategoriesList(array){ 
-    let htmlContentToAppend = ""; //se declara como vacia pero le doy existencia
-    let recorrer_array = array.products; //declaro variable para recorrer los productos del array
-    for(let i = 0; i < recorrer_array.length; i++){ //condicion que recorre cada uno de los elementos del array productos
-        let category = recorrer_array[i]; //variable local que recorre todos los elementos dentro de la condicion del for
-        htmlContentToAppend += `
-        <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
-            <div class="row">
-                <div class="col-3">
-                    <img src="${category.image}" alt="${category.description}" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">${category.name} - ${category.currency} ${category.cost}</h4>
-                        <small class="text-muted">${category.soldCount} vendidos</small>
+const ORDER_ASC_BY_NAME = "AZ"; //asignamos constantes para poder utilizarlas como criterios a la hora de filtrar
+const ORDER_DESC_BY_NAME = "ZA"; //asignamos constantes para poder utilizarlas como criterios a la hora de filtrar
+const ORDER_BY_PROD_COUNT = "Cant."; //asignamos constantes para poder utilizarlas como criterios a la hora de filtrar
+let currentCategoriesArray = []; //asignamos un array vacio.
+let currentSortCriteria = undefined; //asignamos variables de tipo indefinido.
+let minCount = undefined;            //asignamos variables de tipo indefinido.
+let maxCount = undefined;            //asignamos variables de tipo indefinido.
+
+function showCategoriesList(){ //funcion que muestra el listado de categorias 
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentCategoriesArray.products.length; i++){
+        let category = currentCategoriesArray.products[i];
+
+        if (((minCount == undefined) || (minCount != undefined && parseInt(category.cost) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.cost) <= maxCount))){
+            //condicion que evalua los valores de los cammpos del filtrado por costos para devolvernos un nuevo listado
+            htmlContentToAppend += `
+            <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${category.image}" alt="${category.description}" class="img-thumbnail">
                     </div>
-                    <p class="mb-1">${category.description}</p>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${category.name} - ${category.currency} ${category.cost}</h4>
+                            <small class="text-muted">${category.soldCount} artículos</small>
+                        </div>
+                        <p class="mb-1">${category.description}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        `
+            `
+        }
+
         document.getElementById("productos").innerHTML = htmlContentToAppend;
     }
-    document.getElementById("nombre-categorias-bold").innerHTML = `${array.catName}`; //mostramos el nombre de la categoria en pantalla a traves del identificador "nombre-categorias-bold"
+    document.getElementById("nombre-categorias-bold").innerHTML = `${currentCategoriesArray.catName}`;
+}   //mostramos el nombre de cada categoria del array en el elemento con identificador nombre-categrias-array
+
+function sortCategories(criteria, array){ //funcion que ordena las categorias segun criterios a un array dado.
+    let result = [];
+    if (criteria === ORDER_ASC_BY_NAME)
+    {
+        result = array.sort(function(b, a) {  //boton precio de mayor a menor
+            if ( a.cost < b.cost ){ return -1; }
+            if ( a.cost > b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_NAME){ 
+        result = array.sort(function(b, a) { //boton precio de menor a mayor
+            if ( a.cost > b.cost ){ return -1; }
+            if ( a.cost < b.cost ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_BY_PROD_COUNT){ //boton cantidad de articulos de mayor a menor
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
+
+    return result;
 }
- //utilizamos dom para acceder al nodo con identificador "autos101" y agregarle contenido al HTML a traves del JSON
- //El simbolo de ${} hace que el contenido sea un objeto de JS el cual estara dentro del HTML
 
+function setCatID(id) { //funcion que setea el localStorage con el key "CatID, y el valor id"
+    localStorage.setItem("catID", id);
+    window.location = "products.html" // Luego redirecciona a products.html
+}
 
- /*
-EJECUCIÓN:
--Al cargar la página se llama a getJSONData() pasándole por parámetro la dirección para obtener el listado.
--Se verifica el estado del objeto que devuelve, y, si es correcto, se cargan los datos en categoriesArray.
--Por último, se llama a showCategoriesList() pasándole por parámetro categoriesArray.
- 
-*/
+function sortAndShowCategories(sortCriteria, categoriesArray){ //ordenamos segun criterio al array por parametro
+    currentSortCriteria = sortCriteria;
 
-let getDatos = JSON.parse(localStorage.getItem("catID")??[]);
-JSON.stringify(getDatos);
+    if(categoriesArray != undefined){
+        currentCategoriesArray.products = categoriesArray;
+    }
+
+    currentCategoriesArray.products = sortCategories(currentSortCriteria, currentCategoriesArray.products);
+
+    //Muestro las categorías ordenadas
+    showCategoriesList();
+}
+
+let getDatos = JSON.parse(localStorage.getItem("catID")??[]); //Asigno a la variable getDatos, el valor asociado
+// al key de nombre catID a traves del localStorage, y dicho valor lo convierto en un objeto de js.
+JSON.stringify(getDatos); //Ahora tranformo el contenido de getDatos en string
 let setCat = `https://japceibal.github.io/emercado-api/cats_products/${getDatos}.json`;
+//por ultimo asigno a la variable setCat la direccion de los json a trabajar con la variable incluida de getDatos.
 
-document.addEventListener("DOMContentLoaded", ()=>{ //Cuando el documento se carga en la pagina, se executa la funcion flecha.
-    getJSONData(setCat).then(function(resultObj){ //traigo al json en forma de objeto js y despues cree una funcion que tenga como parametro el objeto recibido
-        if (resultObj.status === "ok") //si el resultado es estrictamente igual a ok (status = comprobante si los datos fueron traidos)
-        {
-            categoriesArray = resultObj.data; //actualizo la variable global como variable local y el objeto.data serian los datos del objeto que traje
-            showCategoriesList(categoriesArray); //executo la funcion `showCategoriesList` con los datos obtenidos del objeto como parametro
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(){
+    getJSONData(setCat).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            currentCategoriesArray = resultObj.data
+            showCategoriesList()
+        }else{
+            alert("Algo salió mal: " + resultObj.data);
         }
+    });
+
+    document.getElementById("sortAsc").addEventListener("click", function(){
+        sortAndShowCategories(ORDER_ASC_BY_NAME);
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function(){
+        sortAndShowCategories(ORDER_DESC_BY_NAME);
+    });
+
+    document.getElementById("sortByCount").addEventListener("click", function(){
+        sortAndShowCategories(ORDER_BY_PROD_COUNT);
+    });
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+        document.getElementById("rangeFilterCountMin").value = ""; //vaciamos el contenido
+        document.getElementById("rangeFilterCountMax").value = ""; //vaciamos el contenido
+
+        minCount = undefined; //asignamos undefined a las variables minCount y MaxCount
+        maxCount = undefined; //asignamos undefined a las variables minCount y MaxCount
+
+        showCategoriesList(); //mostramos las categorias sin filtrar
+    });
+
+    document.getElementById("rangeFilterCount").addEventListener("click", function(){
+        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+        //de productos por categoría.
+        minCount = document.getElementById("rangeFilterCountMin").value;
+        maxCount = document.getElementById("rangeFilterCountMax").value;
+
+        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+            minCount = parseInt(minCount);
+        }
+        else{
+            minCount = undefined;
+        }
+
+        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+            maxCount = parseInt(maxCount);
+        }
+        else{
+            maxCount = undefined;
+        }
+
+        showCategoriesList();
     });
 });
