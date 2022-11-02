3f6cc4152bce3f4f6e2716ab762d0a386ec2fc89
M. N. Couto <nahuelcouto@gmail.com>
2022-11-02 16:03:15 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,37 +1,34 @@
 let setFormularioObj = {};
-let getFormularioObj ={};
+let getFormularioObj = {};
+let registro = document.getElementById("registro");
 
 let userMail = localStorage.getItem("userEmail"); //obtenemos a traves del key userMail del LocalStorage el valor mail que ingreso el usuario en el login
 let userMailClean = userMail.substring(1, (userMail.length - 1)); //con el metodo substring quitamos las comillas que nos trae el valor del LocalStorage
 document.getElementById("mail").value = userMailClean; //actualizamos el valor del input de mail con el mail del LocalStorage
 
+// Poner el objeto en el LocalStorage (tipo de dato como string)
 document.getElementById("btnGuardar").addEventListener("click", function() {
 
     setFormularioObj = {
         'nombre': document.getElementById("primerNombre").value, 
-        'apellido': document.getElementById("primerapellido").value, 
+        'apellido': document.getElementById("primerApellido").value, 
         'mail': document.getElementById("mail").value
     };
-
-    // Poner el objeto en el LocalStorage
-    localStorage.setItem('formularioObj', JSON.stringify(setFormularioObj));
     
+    localStorage.setItem('formularioObj', JSON.stringify(setFormularioObj)); 
 });
 
+//Traer el objeto del LocalStorage
 document.addEventListener("DOMContentLoaded", function() {
-
-    //Traer el objeto del LocalStorage
+    
     getFormularioObj = JSON.parse(localStorage.getItem('formularioObj'));
-
-    //Modificar el valor de los input con los valores del LocalStorage.
     document.getElementById("primerNombre").value = getFormularioObj.nombre;
     document.getElementById("primerApellido").value = getFormularioObj.apellido;
 })
 
-//validar formulario
-var registro = document.getElementById("registro");
-
-    registro.addEventListener('submit', function (event) {
+//validacion del formulario.      
+registro.addEventListener('submit', function (event) {
+    
     if (!registro.checkValidity()) {
         event.preventDefault();
         event.stopPropagation();
@@ -40,7 +37,6 @@ var registro = document.getElementById("registro");
     }
     registro.classList.add('was-validated')
 })
-
 
 function validarFormulario() {
     if(registro.checkValidity) {
