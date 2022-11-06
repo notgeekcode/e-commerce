7e22cf1872bff2ead87fe1e9f3f7f2c14679f314
M. N. Couto <nahuelcouto@gmail.com>
2022-11-06 03:55:37 -0300

    Update my-profile.js
    El perfil del usuario mantendra los datos de los inputs a traves del LocalStorage

--- js/my-profile.js
+++ js/my-profile.js
@@ -11,7 +11,10 @@ document.getElementById("btnGuardar").addEventListener("click", function() {
     setFormularioObj = {
         'nombre': document.getElementById("primerNombre").value, 
         'apellido': document.getElementById("primerApellido").value, 
-        'mail': document.getElementById("mail").value
+        'mail': document.getElementById("mail").value,
+        'segundoNombre' : document.getElementById("segundoNombre").value,
+        'segundoApellido' : document.getElementById("segundoapellido").value,
+        'telContacto' : document.getElementById("TelContacto").value
     };
     
     localStorage.setItem(document.getElementById("mail").value, JSON.stringify(setFormularioObj)); //guardamos con el mail como key y en forma de string el objeto en el ls... como el key va ser diferente en cada mail se va a crear un objeto diferente en el ls
@@ -22,8 +25,10 @@ document.getElementById("btnGuardar").addEventListener("click", function() {
 document.addEventListener("DOMContentLoaded", function() {
     //el valor de los input seran iguales a los atributos del objeto de cada usuario segun cada mail.
     document.getElementById("primerNombre").value = JSON.parse(localStorage.getItem(document.getElementById("mail").value)).nombre;
-    document.getElementById("primerApellido").value = JSON.parse(localStorage.getItem(document.getElementById("mail").value)).apellido
-
+    document.getElementById("primerApellido").value = JSON.parse(localStorage.getItem(document.getElementById("mail").value)).apellido;
+    document.getElementById("segundoNombre").value = JSON.parse(localStorage.getItem(document.getElementById("mail").value)).segundoNombre;
+    document.getElementById("segundoapellido").value = JSON.parse(localStorage.getItem(document.getElementById("mail").value)).segundoApellido;
+    document.getElementById("TelContacto").value = JSON.parse(localStorage.getItem(document.getElementById("mail").value)).telContacto;
    
 })
 
